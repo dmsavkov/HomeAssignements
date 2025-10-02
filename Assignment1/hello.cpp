5ac0292d240b006ab0f1a1a6cf6a8e7d6d4d4850
Dmitry Savkov <dmitry_savkov@mail.ru>
2025-10-02 18:10:16 +0300

    Added the loop.

--- Assignment1/hello.cpp
+++ Assignment1/hello.cpp
@@ -3,5 +3,16 @@
 
 void print_hello() {
     std::cout << "Hello world!" << std::endl;
-    return 0;
+
+    bool infinite_loop = true;
+    std::string prefix = "Hello, ";
+    char suffix = '!';
+
+    while (infinite_loop) {
+        std::string input;
+        std::getline(std::cin, input);
+
+        std::string message = prefix + input + suffix;
+        std::cout << message << std::endl;
+    }
 }
